d814bf87f8f622107e69136f25ca2f1d14785585
Dmitrii Kim <mitya_kim@icloud.com>
2022-10-23 21:47:44 -0700

    Initial Commit

---
+++ PizzaWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  PizzaWatch WatchKit Extension
+//
+//  Created by Mitya Kim on 10/23/22.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
